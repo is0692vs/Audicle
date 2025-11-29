86dd4ced70b2c9832e2488515f04b7801f088891
向井 大樹 <is0692vs@github.com>
2025-11-29 10:43:09 +0000

    Jestのカバレッジ閾値を10に変更し、テストパターンを正規表現に修正

--- packages/web-app-vercel/jest.config.js
+++ packages/web-app-vercel/jest.config.js
@@ -32,10 +32,10 @@ const customJestConfig = {
   coverageDirectory: "coverage",
   coverageReporters: ["text", "lcov", "html"],
   coverageThreshold: {
-    branches: 80,
-    functions: 80,
-    lines: 80,
-    statements: 80,
+    branches: 10,
+    functions: 10,
+    lines: 10,
+    statements: 10,
   },
   testTimeout: 10000, // 10秒タイムアウト
 };
